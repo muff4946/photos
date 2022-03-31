ef83c120b835736489f760841e6b23a5a1cfcd78
Anderson <43183321+dbq-andersons@users.noreply.github.com>
2022-03-31 13:28:58 -0500

    Update get_tags_by_image.php

--- api/product/get_tags_by_image.php
+++ api/product/get_tags_by_image.php
@@ -22,6 +22,10 @@ $taglinks = new tag_links($db);
 //get image id from url
 $imageid= isset($_GET['image']) ? $_GET['image'] : '';
 
+$source_page= $_SERVER['REQUEST_URI'};
+
+echo $source_page;
+
 //get array of tag ids
 $stmt = $taglinks->tagsByImage($imageid);
 $num = $stmt->rowCount();
