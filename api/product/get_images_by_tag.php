f146a809179d5c09213a1dac977f4bd9249b4b28
Marcella Anderson <thing1@iastate.edu>
2022-04-01 11:52:52 -0500

    remove a line

--- api/product/get_images_by_tag.php
+++ api/product/get_images_by_tag.php
@@ -31,7 +31,7 @@ $searchType= isset($_GET['searchType']) ? $_GET['searchType'] : '';
 if($searchType == "and"){
 	$stmt = $images->imagesByTagsAnd($tagids, $numTags);
 } else{
-	$numTags++;
+	//$numTags++;
 	echo $numTags;
 	$stmt = $images->imagesByTagsAndExclusive($tagids, $numTags);
 }
