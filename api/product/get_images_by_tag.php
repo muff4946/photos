1cd28b10453bcb02352b66a0151ecf2526ac2f92
dbq-andersons <dbq.andersons@gmail.com>
2022-04-03 16:05:30 -0500

    Update get_images_by_tag.php

--- api/product/get_images_by_tag.php
+++ api/product/get_images_by_tag.php
@@ -30,14 +30,27 @@ $searchType= isset($_GET['searchType']) ? $_GET['searchType'] : '';
 $yearCount= isset($_GET['yearCount']) ? $_GET['yearCount'] : '';
 
 //get array of tag ids
-if($searchType == "and"){
-	$stmt = $images->imagesByTagsAnd($tagids, $numTags);
-} else{
-	if($yearCount == 0){
-		$stmt = $images->imagesByTagsAndExclusiveNoYear($tagids, $numTags);
-	} else{
-		$stmt = $images->imagesByTagsAndExclusive($tagids, $numTags);
-	}
+//if($searchType == "and"){
+//	$stmt = $images->imagesByTagsAnd($tagids, $numTags);
+//} else{
+//	if($yearCount == 0){
+//		$stmt = $images->imagesByTagsAndExclusiveNoYear($tagids, $numTags);
+//	} else{
+//		$stmt = $images->imagesByTagsAndExclusive($tagids, $numTags);
+//	}
+//}
+
+switch ($searchType) {
+	case "and";
+		$stmt = $images->imagesByTagsAnd($tagids, $numTags);
+		break;
+	case "andExclusive";
+		if($yearCount == 0){
+			$stmt = $images->imagesByTagsAndExclusiveNoYear($tagids, $numTags);
+		} else{
+			$stmt = $images->imagesByTagsAndExclusive($tagids, $numTags);
+		}
+		break;
 }
 
 $num = $stmt->rowCount();
