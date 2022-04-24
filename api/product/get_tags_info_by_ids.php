5427f052fe8491c5988214204af2e6d0e2931328
dbq-andersons <dbq.andersons@gmail.com>
2022-04-24 16:41:23 -0500

    Update get_tags_info_by_ids.php

--- api/product/get_tags_info_by_ids.php
+++ api/product/get_tags_info_by_ids.php
@@ -21,6 +21,8 @@ $tags = new tags($db);
 //get image id from url
 $tagid= isset($_GET['tags']) ? $_GET['tags'] : '';
 
+echo $tagid;
+
 //query images
 $stmt = $tags->get_tags_info_by_ids($tagid);
 
