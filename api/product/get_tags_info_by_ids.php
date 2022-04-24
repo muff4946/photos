d25c3684a641a2e4d1d053c4f0507cb68107b275
dbq-andersons <dbq.andersons@gmail.com>
2022-04-24 17:08:18 -0500

    Update get_tags_info_by_ids.php

--- api/product/get_tags_info_by_ids.php
+++ api/product/get_tags_info_by_ids.php
@@ -24,26 +24,43 @@ $tagid= isset($_GET['tags']) ? $_GET['tags'] : '';
 //query images
 $stmt = $tags->get_tags_info_by_ids($tagid);
 
-//retrieve contents
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$num = $stmt->rowCount();
+if ($num>0){
+	$tag_arr = array();
+	
+	$tag_arr["tags"]=array();
+	
+	while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+		
+		extract ($row)
+		
+		$tag_id = $row["tag_id"];
+		$tag_names = $row["tag_names"];
+		$tag_type = $row["tag_type"];
 
-//grab the rows
-$tag_id = $row["tag_id"];
-$tag_names = $row["tag_names"];
-$tag_type = $row["tag_type"];
 
+		$tag_info = array(
+			"id"=>$tag_id,
+			"names"=>$tag_names,
+			"type"=>$tag_type,
+		);
+		array_push ($tag_arr["tags"],$tag_info);
+	}
 
-$tag_info = array(
-	"id"=>$tag_id,
-	"names"=>$tag_names,
-	"type"=>$tag_type,
-);
+	//set response code - 200 OK
+	http_response_code(200);
 
-//set response code - 200 OK
-http_response_code(200);
+	//make it json format
+	echo json_encode($tag_info);
+}
+else{
+	//set response code - 404 Not found
+	http_response_code(404);
+	//tell the user products does not exist
+	echo json_encode(
+		array("message" => "No products found.")
+	);
 
-//make it json format
-echo json_encode($tag_info);
-
+}
 
 ?>
