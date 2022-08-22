75dfa12040b48ac43af5d9e6739dd1944e975508
dbq-andersons <dbq.andersons@gmail.com>
2022-08-21 20:08:27 -0500

    Update tags.php

--- api/objects/tags.php
+++ api/objects/tags.php
@@ -106,12 +106,12 @@ class tags{
 	// get the highest used tag ID number
 	public function get_highest_tag_id(){
 		$query = "select distinct tag_id from tags where tag_type != 'year' order by tag_id desc limit 1";
-		$stmt = $this->connection-prepare($query);
+		$stmt = $this->connection->prepare($query);
 		try{
 			stmt->execute();
 		}
 		catch(PDOException $e){
-			echo stmt . $e->getMessage(();
+			echo stmt . $e->getMessage();
 		}
 		return $stmt;
 	}
