7fe6cec3443c493735f0bfa18f8dca49f92e24ea
dbq-andersons <dbq.andersons@gmail.com>
2022-10-03 19:36:25 -0500

    Update core.php

--- api/config/core.php
+++ api/config/core.php
@@ -6,12 +6,6 @@ error_reporting(E_ALL);
 
 //home page url
 $home_url="https://www.dbq-andersons.com/photos-fork";
-
-function console_log( $data ){
-	echo '<script>';
-	echo 'console.log('. json_encode( $data ) .')';
-	echo '</script>';
-}
   
 // page given in URL parameter, default page is one
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
